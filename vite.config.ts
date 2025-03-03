4e7acc509559c607b7570b3a0c251be9150e75fc
munawiki <munawiki@gmail.com>
2025-03-03 13:00:47 +0900

    ✨ (vite.config.ts): add new path aliases for components, utils, UI, lib, and hooks to improve code organization and import clarity

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,12 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       '@core': path.resolve(__dirname, './src/core'),
       '@features': path.resolve(__dirname, './src/features'),
-      '@assets': path.resolve(__dirname, './src/assets')
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@utils': path.resolve(__dirname, './src/lib/utils'),
+      '@ui': path.resolve(__dirname, './src/components/ui'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@hooks': path.resolve(__dirname, './src/hooks')
     }
   }
 })
